5974843967d04977edb172f3c1a6cf28afda6c35
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-02-07 10:22:59 +0100

    Implement book registration functionality

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
-//SELECTED ELEMENTS
+const books = [];
+const audiobooks = [];
+
+//SELECT ELEMENTS
 const audioButton = document.querySelector('.audio-button');
 const physicalButton = document.querySelector('.physical-button');
-const selectElement = document.querySelector('.select');
-const isbnInput = document.querySelector('.isbn');
-const narratorInput = document.querySelector('.narrator')
+const selectElement = document.querySelector('.format');
+const isbn = document.querySelector('.isbn');
+const narrator = document.querySelector('.narrator');
+const registerForm = document.querySelector('.book-form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
 
-//SELECT ELEMENT
+//SELECT FORMAT
 selectElement.addEventListener('change', ()=> {
 	if (selectElement.value === 'physical') {
-		isbnInput.removeAttribute('disabled');
-		narratorInput.setAttribute('disabled', '');
+		isbn.removeAttribute('disabled');
+		narrator.setAttribute('disabled', '');
 	} else {
-		isbnInput.setAttribute('disabled', '');
-		narratorInput.removeAttribute('disabled');
+		isbn.setAttribute('disabled', '');
+		narrator.removeAttribute('disabled');
 	}
 });
+
+registerForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let newBook;
+	if (selectElement.value === 'physical') {
+		newBook = new Book (
+			title.value,
+			author.value,
+			selectElement.value,
+			isbn.value
+		);
+	} else {
+		newBook = new Audiobook(
+			title.value,
+			author.value,
+			selectElement.value,
+			narrator.value
+		);
+	}
+	Book.addBook(newBook);
+	console.log(newBook);
+	console.log(books);
+	console.log(audiobooks);
+})
 
 //DECLARING PHYSICAL BOOK CLASS
 class Book {
@@ -25,10 +55,17 @@ class Book {
 		this.ibsn = isbn;
 		this.ID = Date.now();
 	}
+	static addBook(book) {
+		if (book.format === 'physical') {
+			books.push(book);
+		} else {
+			audiobooks.push(book)
+		}
+	}
 }
 
 //DECLARING AUDIO BOOK CLASS
-class AudioBook extends Book {
+class Audiobook extends Book {
 	constructor(title, author, format, narrator) {
 		super(title, author, format);
 		this.narrator = narrator;
